13a98805cc2bb6ee69f2c148f1c9afac1ccbc02d
greenbush5 <dianadoenko5@gmail.com>
2024-05-05 02:57:25 +0300

    split `values` into definitions and impls

--- crates/prog-interpreter/src/values/impls.rs
+++ crates/prog-interpreter/src/values/impls.rs
@@ -1,35 +1,6 @@
-use std::fmt::Display;
-use std::collections::HashMap;
+use super::*;
 
-use anyhow::Result;
-use serde::Serialize;
-
-use prog_parser::ast;
-use prog_macros::{VariantUnwrap, EnumKind};
-
-use crate::arg_parser::{ArgList, ParsedArg};
-use crate::context::RuntimeContext;
-
-#[derive(Debug, Clone, PartialEq, Serialize, VariantUnwrap, EnumKind)]
-pub enum RuntimeValue {
-	Boolean(bool),
-	String(String),
-	Number(f64),
-	List(Vec<RuntimeValue>),
-	Object(HashMap<String, RuntimeValue>),
-
-	#[serde(serialize_with = "serde_use_display")]
-	Function(RuntimeFunction),
-	
-	#[serde(serialize_with = "serde_use_display")]
-	IntrinsicFunction(IntrinsicFunction),
-	
-	// It is of type `Identifier` mainly to avoid `TryInto<String>` conflicts with `String` variant in `Conversion` derive macro
-	#[serde(skip)]
-	Identifier(Identifier),
-
-	Empty
-}
+//* From<T> *//
 
 impl From<ast::expressions::Literal> for RuntimeValue {
 	fn from(value: ast::expressions::Literal) -> Self {
@@ -42,6 +13,14 @@ impl From<ast::expressions::Literal> for RuntimeValue {
 		}
 	}
 }
+
+impl From<String> for Identifier {
+	fn from(value: String) -> Self {
+		Self(value)
+	}
+}
+
+//* Display *//
 
 impl Display for RuntimeValue {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -81,19 +60,6 @@ impl Display for RuntimeValue {
 	}
 }
 
-fn serde_use_display<T: Display, S: serde::Serializer>(value: &T, serializer: S) -> std::result::Result<S::Ok, S::Error> {
-	serializer.collect_str(value)
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct RuntimeFunction {
-	pub arguments: Vec<String>,
-	pub statements: Vec<ast::Statement>,
-
-	pub source: String,
-	pub file: String
-}
-
 impl Display for RuntimeFunction {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		let arguments_str = self.arguments.join(", ");
@@ -103,36 +69,8 @@ impl Display for RuntimeFunction {
 	}
 }
 
-pub type IntrinsicFunctionPtr = fn(
-	context: &mut RuntimeContext,
-	args: HashMap<String, ParsedArg>,
-	call_site: CallSite
-) -> Result<RuntimeValue>;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct IntrinsicFunction {
-	pub pointer: IntrinsicFunctionPtr,
-	pub arguments: ArgList
-}
-
 impl Display for IntrinsicFunction {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(f, "func({:?})", self.pointer)
-	}
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct CallSite {
-	pub source: String,
-	pub file: String,
-	pub position: ast::Position
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Identifier(pub String);
-
-impl From<String> for Identifier {
-	fn from(value: String) -> Self {
-		Self(value)
 	}
 }
---
+++ crates/prog-interpreter/src/values/mod.rs
@@ -0,0 +1,69 @@
+mod impls;
+
+use std::fmt::Display;
+use std::collections::HashMap;
+
+use anyhow::Result;
+use serde::Serialize;
+
+use prog_parser::ast;
+use prog_macros::{VariantUnwrap, EnumKind};
+
+use crate::arg_parser::{ArgList, ParsedArg};
+use crate::context::RuntimeContext;
+
+#[derive(Debug, Clone, PartialEq, Serialize, VariantUnwrap, EnumKind)]
+pub enum RuntimeValue {
+	Boolean(bool),
+	String(String),
+	Number(f64),
+	List(Vec<RuntimeValue>),
+	Object(HashMap<String, RuntimeValue>),
+
+	#[serde(serialize_with = "serde_use_display")]
+	Function(RuntimeFunction),
+	
+	#[serde(serialize_with = "serde_use_display")]
+	IntrinsicFunction(IntrinsicFunction),
+	
+	// It is of type `Identifier` mainly to avoid `TryInto<String>` conflicts with `String` variant in `Conversion` derive macro
+	#[serde(skip)]
+	Identifier(Identifier),
+
+	Empty
+}
+
+fn serde_use_display<T: Display, S: serde::Serializer>(value: &T, serializer: S) -> std::result::Result<S::Ok, S::Error> {
+	serializer.collect_str(value)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct RuntimeFunction {
+	pub arguments: Vec<String>,
+	pub statements: Vec<ast::Statement>,
+
+	pub source: String,
+	pub file: String
+}
+
+pub type IntrinsicFunctionPtr = fn(
+	context: &mut RuntimeContext,
+	args: HashMap<String, ParsedArg>,
+	call_site: CallSite
+) -> Result<RuntimeValue>;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IntrinsicFunction {
+	pub pointer: IntrinsicFunctionPtr,
+	pub arguments: ArgList
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CallSite {
+	pub source: String,
+	pub file: String,
+	pub position: ast::Position
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Identifier(pub String);
