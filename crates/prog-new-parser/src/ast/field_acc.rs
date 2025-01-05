514c34f3798dc711f926efbea40f699c29aa05e4
greenbush5 <29354804+im-fiv@users.noreply.github.com>
2025-01-05 15:21:02 +0300

    added `Parse` impls to `FieldAcc` and `IndexAcc`

--- crates/prog-new-parser/src/ast/field_acc.rs
+++ crates/prog-new-parser/src/ast/field_acc.rs
@@ -1,9 +1,37 @@
+use anyhow::Result;
+
 use crate::ast::*;
-use crate::token;
+use crate::{token, ASTNode, Parse, ParseStream, Position, Span};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct FieldAcc<'inp> {
 	pub object: Box<Term<'inp>>,
 	pub _dot: token::Dot<'inp>,
 	pub field: token::Ident<'inp>
+}
+
+impl<'inp> ASTNode<'inp> for FieldAcc<'inp> {
+	fn span(&'inp self) -> prog_utils::pretty_errors::Span<'inp> {
+		let start = self.object.span().start();
+		let end = self.field.span().end();
+
+		let source = self.object.span().source();
+		let position = Position::new(start, end);
+
+		Span::new(source, position)
+	}
+}
+
+impl<'inp> Parse<'inp> for FieldAcc<'inp> {
+	fn parse(input: &'_ ParseStream<'inp>) -> Result<Self> {
+		let object = Box::new(input.parse::<Term>()?);
+		let _dot = input.parse::<token::Dot>()?;
+		let field = input.parse::<token::Ident>()?;
+
+		Ok(Self {
+			object,
+			_dot,
+			field
+		})
+	}
 }
--- crates/prog-new-parser/src/ast/index_acc.rs
+++ crates/prog-new-parser/src/ast/index_acc.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
+
 use crate::ast::*;
-use crate::token;
+use crate::{token, ASTNode, Parse, ParseStream, Position, Span};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct IndexAcc<'inp> {
@@ -7,4 +9,32 @@ pub struct IndexAcc<'inp> {
 	pub _lb: token::LeftBracket<'inp>,
 	pub index: Box<Expr<'inp>>,
 	pub _rb: token::RightBracket<'inp>
+}
+
+impl<'inp> ASTNode<'inp> for IndexAcc<'inp> {
+	fn span(&'inp self) -> Span<'inp> {
+		let start = self.list.span().start();
+		let end = self._rb.span().end();
+
+		let source = self.list.span().source();
+		let position = Position::new(start, end);
+
+		Span::new(source, position)
+	}
+}
+
+impl<'inp> Parse<'inp> for IndexAcc<'inp> {
+	fn parse(input: &'_ ParseStream<'inp>) -> Result<Self> {
+		let list = Box::new(input.parse::<Term>()?);
+		let _lb = input.parse::<token::LeftBracket>()?;
+		let index = Box::new(input.parse::<Expr>()?);
+		let _rb = input.parse::<token::RightBracket>()?;
+
+		Ok(Self {
+			list,
+			_lb,
+			index,
+			_rb
+		})
+	}
 }
