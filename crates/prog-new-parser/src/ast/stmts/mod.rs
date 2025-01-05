3a359d1e5221f482e3f7e40fc4b3e53465435cc8
greenbush5 <29354804+im-fiv@users.noreply.github.com>
2025-01-05 16:14:45 +0300

    changes to statements

--- crates/prog-new-parser/src/ast/stmts/mod.rs
+++ crates/prog-new-parser/src/ast/stmts/mod.rs
@@ -1,3 +1,45 @@
 mod var_def;
+pub use var_def::VariableDefinition;
 
-pub use var_def::*;
+use crate::{ast, ASTNode, Position, Span};
+
+#[derive(Debug)]
+pub struct Program<'inp> {
+	pub statements: Vec<Statement<'inp>>
+}
+
+#[derive(Debug)]
+pub enum Statement<'inp> {
+	VariableDefinition(VariableDefinition<'inp>),
+	FunctionCall(ast::Call<'inp>)
+}
+
+impl ASTNode<'_> for Program<'_> {
+	fn span(&self) -> Span {
+		assert!(
+			!self.statements.is_empty(),
+			"Could not get program's span as it is empty"
+		);
+
+		let first = self.statements.first().unwrap().span();
+		let last = self.statements.last().unwrap().span();
+
+		let start = first.start();
+		let end = last.end();
+
+		let source = first.source();
+		let position = Position::new(start, end);
+
+		Span::new(source, position)
+	}
+}
+
+impl ASTNode<'_> for Statement<'_> {
+	fn span(&self) -> Span {
+		match self {
+			Self::VariableDefinition(stmt) => stmt as &dyn ASTNode,
+			Self::FunctionCall(stmt) => stmt as &dyn ASTNode
+		}
+		.span()
+	}
+}
